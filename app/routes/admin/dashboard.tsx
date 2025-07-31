6a50d8648b32a3e3c2405aed6574b3ce351abb57
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-07-31 23:55:15 +0100

    Working on dashboard UI components

--- app/routes/admin/dashboard.tsx
+++ app/routes/admin/dashboard.tsx
@@ -1,14 +1,44 @@
-import { Header } from "components"
+import { Header, StatsCard, TripCard } from "components"
 
 const Dashboard = () => {
     const user = {name: "Yarik"};
+    const dashboardStats = {
+        totalUsers: 12450,
+        usersJoined: {currentMonth: 218, lastMonth: 168},
+        totalTrips: 2580,
+        tripsCreated: {currentMonth: 150, lastMonth: 250 },
+        userRole: {total: 62, currentMonth: 25, lastMonth: 15}
+    }
     return(
         <main className="dashboard wrapper">
             <Header 
                 title = {`Welcome ${user?.name ?? 'Guest'}`}
                 description="Track activity, trends and popular destinations in real time"
             />
-            Dasboard Page contents
+            <section className = "flex flex-col gap-6">
+                <div className = "grid grid-cols-1 md:grid-cols-3 gap-6w-full">
+                    <StatsCard
+                        headerTitle = "Total Users" 
+                        total = {dashboardStats.totalUsers}
+                        currentMonthCount = {dashboardStats.usersJoined.currentMonth}
+                        lastMonthCount = {dashboardStats.usersJoined.lastMonth}
+                    />
+                    <StatsCard
+                        headerTitle = "Total Trips" 
+                        total = {dashboardStats.totalTrips}
+                        currentMonthCount = {dashboardStats.tripsCreated.currentMonth}
+                        lastMonthCount = {dashboardStats.tripsCreated.lastMonth}
+                    />
+                    <StatsCard
+                        headerTitle = "Active Users Today" 
+                        total = {dashboardStats.userRole.total}
+                        currentMonthCount = {dashboardStats.userRole.currentMonth}
+                        lastMonthCount = {dashboardStats.userRole.lastMonth}
+                    />
+                </div>
+            </section>
+            <StatsCard />
+            <TripCard />
         </main>
     )
 }
