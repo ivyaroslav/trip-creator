5f1daa4af84bbe2d9ad13215bba406633e97353d
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-08-03 23:27:18 +0100

    Authentication is mostly done

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,6 +1,24 @@
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import { MobileSidebar, NavItems } from "components";
-import {Outlet} from "react-router";
+import {Outlet, redirect} from "react-router";
+import { getExistingUser, storeUserData } from "~/appwrite/auth";
+import { account } from "~/appwrite/client";
+export async function clientLoader() {
+    try {
+        const user = await account.get();
+
+        if(!user.$id) return redirect ('/sign-in');
+        const existingUser = await getExistingUser(user.$id);
+        if(existingUser?.status === 'user') {
+            return redirect ('/');
+        }
+
+        return existingUser?.$id ? existingUser : await storeUserData();
+    } catch(e) {
+        console.log('Error fetching user', e)
+        return redirect('/sign-in')
+    }
+}
 const AdminLayout = () => {
     return(
         <div className = "admin-layout" >
