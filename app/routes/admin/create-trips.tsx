5c608485b05b0370089d5d4deda890944e0323d0
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-08-13 22:38:28 +0100

    UI of the generating trip form done

--- app/routes/admin/create-trips.tsx
+++ app/routes/admin/create-trips.tsx
@@ -1,6 +1,12 @@
 import { ComboBoxComponent } from "@syncfusion/ej2-react-dropdowns";
 import Header from "components/Header"
 import type { Route } from "./+types/create-trips";
+import { comboBoxItems, selectItems } from "~/constants";
+import { formatKey } from "lib/utils";
+import { LayerDirective, LayersDirective, MapsComponent } from "@syncfusion/ej2-react-maps";
+import { useState } from "react";
+import { world_map } from "~/constants/world_map";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 
 export const loader = async () => {
     const response = await fetch('https://restcountries.com/v3.1/all?fields=name,latlng,flag,maps');
@@ -17,13 +23,36 @@ export const loader = async () => {
 }
 
 const CreateTrips = ({loaderData}: Route.ComponentProps) => {
-    const handleSubmit = async () => {};
-    const handleChange = (key: keyof TripFormData, value: string | number) => {}
     const countries = loaderData as Country[];
+    const [formData, setFormData] = useState<TripFormData>({
+        country: countries[0]?.name || '',
+        travelStyle: '',
+        interest: '',
+        budget: '',
+        duration: 0,
+        groupType: ''
+    });
+    const [error, setError] = useState<string | null>(null);
+    const [loading, setLoading] = useState(false);
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault()
+        setLoading(true);
+    };
+    const handleChange = (key: keyof TripFormData, value: string | number) => {
+        setFormData({...formData, [key]: value})
+    }
+    
     const countryData = countries.map((country) => ({
         text: country.name,
         value: country.value,
     }))
+    const mapData = [
+        {
+            country: formData.country,
+            color: '#EA382E',
+            coordinates: countries.find((c: Country) => c.name === formData.country)?.coordinates || []
+        }
+    ]
     return (
         <main className = "flex flex-col gap-10 pb-20 wrapper">
             <Header title = "Add a New Trip" description = "View and Edit AI-generated travel plans"/>
@@ -64,8 +93,80 @@ const CreateTrips = ({loaderData}: Route.ComponentProps) => {
                             id = "duration"
                             name = "duration"
                             placeholder = "Enter a number of days (5,12).."
+                            className = "form-input placeholder:text-gray-100"
+                            onChange = {(e) => handleChange('duration', Number(e.target.value))}
                         ></input>
                     </div>
+
+                    {selectItems.map((key) =>(
+                        <div key={key}>
+                            <label htmlFor = {key}>{formatKey(key)}</label>
+                            <ComboBoxComponent
+                                id = {key}
+                                dataSource = {comboBoxItems[key].map((item) => ({
+                                    text: item,
+                                    value: item,
+                                }))} 
+                                fields = {{text: 'text', value: 'value'}}
+                                placeholder = {`Select ${formatKey(key)}`}
+                                change = {(e: {value: string | undefined}) => {
+                                    if(e.value) {
+                                        handleChange(key, e.value)
+                                    }
+                                }}
+                                allowFiltering
+                                filtering = {(e) => {
+                                    const query = e.text.toLowerCase();
+                                    e.updateData(
+                                        comboBoxItems[key]
+                                        .filter((item) => item.toLowerCase().includes(query)).map(((item) => 
+                                        ({
+                                            text: item,
+                                            value: item,
+                                        }))
+                                    ))
+                                }}
+                                className = "combo-box"
+                            />
+
+                        </div>
+                    ))}
+
+                    <div>
+                        <label htmlFor = "location">
+                            Location on the world map
+                        </label>
+                        <MapsComponent>
+                            <LayersDirective>
+                                <LayerDirective
+                                    shapeData={world_map}
+                                    dataSource={mapData}
+                                    shapePropertyPath="name"
+                                    shapeDataPath="country"
+                                    shapeSettings={{colorValuePath: 'color', fill: '#e5e5e5'} }
+                                />
+                            </LayersDirective>
+                        </MapsComponent>
+                    </div>
+
+                    <div className = "bg-gray-200 h-px w-full"/>
+                    {error && (
+                        <div className = "error">
+                            <p>{error}</p>
+                        </div>
+                    )}
+                    <footer className="px-6 w-full">
+                        <ButtonComponent
+                            type = "submit"
+                            className = "button-class !h-12 !w-full"
+                            disabled = {loading}
+                        >
+                            <img src = {`/assets/icons/${loading ? 'loader.svg' : 'magic-star.svg'}`}/>
+                            <span className = "p-16-semibold text-white">
+                                {loading ? 'Generating...' : 'Generate Trip'}
+                            </span>
+                        </ButtonComponent>
+                    </footer>
                 </form>
             </section>
         </main>
