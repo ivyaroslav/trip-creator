a618aa7b1fa8d4004ae199aed22f058c39b1bfbb
yaroslav.iv <yarikivanitsa39@gmail.com>
2025-08-26 13:41:27 +0100

    Generating trip functionality done

--- app/routes/admin/create-trips.tsx
+++ app/routes/admin/create-trips.tsx
@@ -8,6 +8,7 @@ import { useState } from "react";
 import { world_map } from "~/constants/world_map";
 import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 import { account } from "~/appwrite/client";
+import { useNavigate } from "react-router";
 
 export const loader = async () => {
     const response = await fetch('https://restcountries.com/v3.1/all?fields=name,latlng,flag,maps');
@@ -25,6 +26,7 @@ export const loader = async () => {
 
 const CreateTrips = ({loaderData}: Route.ComponentProps) => {
     const countries = loaderData as Country[];
+    const navigate = useNavigate();
     const [formData, setFormData] = useState<TripFormData>({
         country: countries[0]?.name || '',
         travelStyle: '',
@@ -64,9 +66,25 @@ const CreateTrips = ({loaderData}: Route.ComponentProps) => {
         }
 
         try {
-            console.log('user', user);
-            console.log('formData', formData);
-        } catch(e) {
+           const response = await fetch('/api/create-trip',
+           {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                country: formData.country,
+                numberOfDays: formData.duration,
+                travelStyle: formData.travelStyle,
+                interests: formData.interest,
+                budget: formData.budget,
+                groupType: formData.groupType,
+                userId: user.$id
+            })
+           })
+           const result: CreateTripResponse = await response.json();
+
+           if(result?.id) navigate(`/trips/${result.id}`)
+           else console.error('Failed to generate a trip');
+        }catch(e) {
             console.error('Error of generating a trip', e);
         } finally {
             setLoading(false)
